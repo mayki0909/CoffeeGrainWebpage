41e8890c160f7ec0ea0dc2dfc8b37866374e876c
Miha Žnidar <mayki.znidar@gmail.com>
2022-07-29 11:56:55 +0200

    Professions style

--- components/professions/professions.tsx
+++ components/professions/professions.tsx
@@ -8,9 +8,10 @@ export const Professions: NextComponentType = () => {
     <section id='about' className='backgroundWhite'>
       <div className='container pt-5'>
         <div className='row'>
-          <div className='col-xs-12 col-lg-6'>
+          <div className='col-xs-12 col-lg-4'>
             <div className={styles.card}>
-              <p className={styles.title}>GRAPHIC DESIGN</p>
+              <img className={styles.image} src="/images/Miha.png" alt="" />
+              <p className={styles.title}>Graphic design</p>
               <p className={styles.description}>
                 Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, 
                 lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, 
@@ -18,9 +19,21 @@ export const Professions: NextComponentType = () => {
               </p>
             </div>
           </div>
-          <div className='col-xs-12 col-lg-6'>
+          <div className='col-xs-12 col-lg-4'>
             <div className={styles.card}>
-              <p className={styles.title}>WEB SOLUTIONS</p>
+              <img className={styles.image} src="/images/Miha.png" alt="" />
+              <p className={styles.title}>Web solutions</p>
+              <p className={styles.description}>
+                Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, 
+                lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, 
+                lorem ipsum, lorem ipsum, lorem ipsum 
+              </p>
+            </div>
+          </div>
+          <div className='col-xs-12 col-lg-4'>
+            <div className={styles.card}>
+              <img className={styles.image} src="/images/Miha.png" alt="" />
+              <p className={styles.title}>Neki neki</p>
               <p className={styles.description}>
                 Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, 
                 lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, 
